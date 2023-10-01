79651bbfab9d70e5cb43f28dcb4e507563090380
zakariabatlim <zakaria.batlim@gmail.com>
2023-10-01 12:56:21 +0100

    final session 1

---
+++ Monscriptjava.js
@@ -0,0 +1,7 @@
+function MasuperFunction(){
+    alert("hello world");
+    let Titre = document.getElementById("montitre");
+    Titre.innerHTML =  "hello ce texte est en JS";
+ }
+ let Mysecondbutton =document.getElementById("Mybtn")
+ Mysecondbutton.addEventListener("click", MasuperFunction)
